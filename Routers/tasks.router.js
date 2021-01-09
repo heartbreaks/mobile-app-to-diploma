448052a5c92328f3fd020d4e637ee81abd61d792
Sergey Khokhlov <sergeyxoxlov123@gmail.com>
2021-01-10 04:53:56 +0500

    added navigation after press on task with their info added api for add card added fix in AppNavigator

--- Routers/tasks.router.js
+++ Routers/tasks.router.js
@@ -1,16 +1,18 @@
 const { Router } = require("express");
 const Tasks = require("../Models/Tasks");
 const router = Router();
-
+const { check, validationResult } = require("express-validator");
 // localhost:5000/tasks
 router.get("/", async (req, res) => {
   try {
     const { executor } = req.query;
     console.log(executor);
     const tasks = await Tasks.findAll({ where: { executor: executor } });
-    
+
     if (!tasks) {
-      return res.status(400).json({ message: "Tasks with this executor not find" });
+      return res
+        .status(400)
+        .json({ message: "Tasks with this executor not find" });
     }
 
     res.status(200).json({ message: "Tasks found", tasks });
@@ -19,20 +21,23 @@ router.get("/", async (req, res) => {
   }
 });
 
-// router.post("/add", async (req, res) => {
-//   try {
-//     const { executor, title, body, date, level_primary } = req.query;
+router.post("/add", async (req, res) => {
+  try {
+    const { executor, title, body, date, level_primary } = req.query;
 
-//     const tasks = await Tasks.findAll({ where: { executor: executor } });
+    const task = new Tasks({
+      executor,
+      title,
+      body,
+      date,
+      level_primary,
+    });
 
-//     if (tasks) {
-//       return res.status(400).json({ message: "Tasks with this executor not find" });
-//     }
-
-//     res.status(201).json({ message: "Register was succeshul" });
-//   } catch (err) {
-//     res.status(500).json({ message: "Вывалилась ошибка", err: err });
-//   }
-// });
+    await task.save();
+    res.status(201).json({ message: "Task has been added" });
+  } catch (err) {
+    res.status(500).json({ message: "Вывалилась ошибка", err: err });
+  }
+});
 
 module.exports = router;
