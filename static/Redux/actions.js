ad7920b8879ef3da34ec4e93cd0d7ecc55251405
heartbreak <sergeykhokhlov12345678@gmail.com>
2021-01-15 13:50:43 +0500

    Add new task

--- static/Redux/actions.js
+++ static/Redux/actions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { setStatusBarBackgroundColor } from "expo-status-bar";
 import { Alert } from "react-native";
-import { AUTH_USER, GET_EMPLOYERS } from "./types";
+import { AUTH_USER, GET_EMPLOYERS, ADD_NEW_TASK } from "./types";
 
 export const toLogin = (login, password) => {
   return async dispatch => {
@@ -10,7 +11,7 @@ export const toLogin = (login, password) => {
       );
 
       const users = await axios.get(`http://192.168.1.6:5000/employers`);
-      
+
       dispatch({
         type: AUTH_USER,
         response: res,
@@ -37,6 +38,20 @@ export const getTasks = userId => {
         type: AUTH_USER,
         response: res,
       });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const addNewTask = dataFlow => {
+  return async dispatch => {
+    try {
+      const res = await axios.post(
+        `http://192.168.1.6:5000/tasks/add?executor=${dataFlow.executor}&title=${dataFlow.title}&body=${dataFlow.body}&date=${dataFlow.date}&level_primary=${dataFlow.levelPrimary}`
+      );
+      console.log("Add");
+      return { code: 200, msg: "Success" };
     } catch (err) {
       console.log(err);
     }
--- static/Screens/AddNewTaskScreen.js
+++ static/Screens/AddNewTaskScreen.js
@@ -8,17 +8,42 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { connect } from "react-redux";
+import { addNewTask } from "../Redux/actions";
 import RNPickerSelect from "react-native-picker-select";
+import DatePicker from "react-native-datepicker";
+import { bindActionCreators } from "redux";
 
 function AddNewTaskScreen(props) {
   const { employers } = props;
-  console.log(employers);
+
+  const [title, setTitle] = React.useState("");
+  const [body, setBody] = React.useState("");
+  const [executor, setExecutor] = React.useState("");
+  const [date, setDate] = React.useState(currentDate());
+  const [levelPrimary, setLevelPrimary] = React.useState("");
+
+  function currentDate() {
+    let dateNow = new Date().toLocaleTimeString("ru-ru", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
+
+    let dateNowArr = dateNow.split(".");
+    dateNow = `${dateNowArr[2]}-${dateNowArr[1]}-${dateNowArr[0]}`;
+    console.log(dateNow);
+    return dateNow; 
+  }
 
   return (
     <View style={styles.container}>
-      <View>
+      <View style={{ maxWidth: "90%" }}>
         <Text>Заполните поля ниже для создания задачи</Text>
-        <TextInput style={styles.login} placeholder="Введите название задачи" />
+        <TextInput
+          onChangeText={title => setTitle(title)}
+          style={styles.login}
+          placeholder="Введите название задачи"
+        />
         <View style={styles.textAreaContainer}>
           <TextInput
             style={styles.textArea}
@@ -26,28 +51,55 @@ function AddNewTaskScreen(props) {
             placeholder="Напишите описание задачи"
             placeholderTextColor="grey"
             numberOfLines={10}
+            onChangeText={body => setBody(body)}
             multiline={true}
           />
         </View>
         <View style={styles.textAreaContainer}>
           <RNPickerSelect
-            onValueChange={value => console.log(value)}
+            onValueChange={employer => setExecutor(employer)}
             placeholder={"Select employer"}
             items={employers}
           />
         </View>
         <View style={styles.textAreaContainer}>
           <RNPickerSelect
-            onValueChange={value => console.log(value)}
+            onValueChange={levelPrimary => setLevelPrimary(levelPrimary)}
             placeholder={"Select level of task"}
             items={[
-              {label: 'Скоро дедлайн', value: 2},
-              {label: 'Нормальный', value: 1},
-              {label: 'Срочно', value: 3},
+              { label: "Скоро дедлайн", value: 2 },
+              { label: "Нормальный", value: 1 },
+              { label: "Срочно", value: 3 },
             ]}
           />
         </View>
-        <Text>Создать задачу</Text>
+        <View style={{ marginBottom: 5 }}>
+          <DatePicker
+            style={{ width: "100%" }}
+            date={date}
+            mode="date"
+            placeholder="Введите срок сдачи"
+            format="YYYY-MM-DD"
+            minDate={date}
+            maxDate="2030-12-31"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            onDateChange={date => {
+              setDate(date);
+            }}
+          />
+        </View>
+        <TouchableOpacity
+          onPress={() => {
+            let data = { title, body, executor, levelPrimary, date };
+            props.addNewTask(data);
+            console.log(data);
+          }}
+        >
+          <View style={styles.boxShadow}>
+            <Text style={{ textAlign: "center" }}>Создать задачу</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -63,7 +115,7 @@ const styles = StyleSheet.create({
   login: {
     borderBottomColor: "#000",
     color: "black",
-    borderBottomWidth: 2,
+    borderBottomWidth: 1,
     marginTop: 20,
     marginBottom: 5,
     paddingBottom: 3,
@@ -72,12 +124,23 @@ const styles = StyleSheet.create({
     borderColor: "black",
     borderWidth: 1,
     padding: 5,
-    maxWidth: "90%",
     marginBottom: 5,
   },
   textArea: {
     height: 150,
     justifyContent: "flex-start",
+  },
+  boxShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9,
+    backgroundColor: "#f5cb5c",
+    padding: 10,
   },
 });
 
@@ -86,5 +149,7 @@ function mapStateToProps(state) {
     employers: state.humans,
   };
 }
-
-export default connect(mapStateToProps)(AddNewTaskScreen);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ addNewTask }, dispatch);
+}
+export default connect(mapStateToProps, mapDispatchToProps)(AddNewTaskScreen);
