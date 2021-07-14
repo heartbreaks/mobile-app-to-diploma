a4f6a6fd678d934521ec22d56b5e196cd0a1e2b1
Sergey Khokhlov <sergeykhokhlov12345678@gmail.com>
2021-07-14 07:03:10 +0500

    test

--- databaseSettings.js
+++ databaseSettings.js
@@ -5,10 +5,3 @@ module.exports = new Sequelize('heroku_d60c203efce6810', 'mysql', 'mysql', {
     host: 'localhost',
     dialect: 'mysql',
 })
-/*
-module.exports = new Sequelize('heroku_dbf3f42dc9689a9', 'b6fcff8f164fd6', '42c359d3', {
-    host: 'eu-cdbr-west-01.cleardb.com',
-
-    dialect: 'mysql'
-})
-*/
