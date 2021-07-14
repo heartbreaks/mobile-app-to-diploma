4c5f9e4b1c593c56a21e4dac101e628127b7961a
Sergey Khokhlov <sergeykhokhlov12345678@gmail.com>
2021-07-14 06:43:24 +0500

    'Something'

--- Controllers/authController.js
+++
@@ -1,8 +0,0 @@
-module.exports.login = function(req, res) {
-    res.status(200).json({
-        login: {
-            login: req.body.login,
-            password: req.body.password
-        }
-    })
-}
