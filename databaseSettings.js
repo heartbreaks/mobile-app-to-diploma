1b59c56fb193aa835a9685705913ed0aba1f8bb2
heartbreaks_f <sergeykhokhlov12345678@gmail.com>
2023-11-18 22:12:05 +0500

    Fix :D

--- databaseSettings.js
+++ databaseSettings.js
@@ -1,9 +1,19 @@
 const Sequelize = require('sequelize')
 
 
-module.exports = new Sequelize('heroku_d60c203efce6810', 'mysql', 'mysql', {
-    host: 'localhost',
+module.exports = new Sequelize('railway', 'root', 'c66-EgFDf62bh5gGFhEha6EaGfHcEgCF', {
+    host: 'viaduct.proxy.rlwy.net',
+    port: 57049,
     dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+    dialectOptions: {
+        timeout: 60000, // Установите значение тайм-аута в миллисекундах
+    }
 })
 /*
 module.exports = new Sequelize('heroku_dbf3f42dc9689a9', 'b6fcff8f164fd6', '42c359d3', {
